9cf096b65ce11bf6d36c9572538250dde45e0a81
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-03-16 17:52:52 +0100

    ajout icon browser favicon

--- _footer.php
+++ _footer.php
@@ -1,7 +1,16 @@
 
 
 <link rel="stylesheet" href="./assets/style.css">
-    <footer>copyright hello footer</footer>
+    <footer>
+        <div>© dePhants Team </div>
+        <div>Mentions Legales</div>
+        <div class:"rezo">
+            <img src="https://fakeimg.pl/20x20/?text=F">
+            <img src="https://fakeimg.pl/20x20/?text=T">
+            <img src="https://fakeimg.pl/20x20/?text=M">
+        </div>
+        
+    </footer>
 
 </html>
 
--- _header.php
+++ _header.php
@@ -1,1 +1,9 @@
-"hello header"
+<link rel="stylesheet" href="./assets/style.css">
+
+<nav>
+    
+    <div>"navbar"</div>
+    
+</nav>
+<img id="logo" src="https://fakeimg.pl/100x100/?text=Hello">
+</html>
