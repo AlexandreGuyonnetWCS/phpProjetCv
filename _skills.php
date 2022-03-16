e1189530d0e43ae075cac67a9d0fb41b4161dab7
TUSHAR CHOWDHURY <tushar.97chowdhury@gmail.com>
2022-03-16 18:02:07 +0100

    added skills section

--- _skills.php
+++ _skills.php
@@ -1,1 +1,40 @@
-"hello skills"
+<link rel="stylesheet" href="./assets/style.css">
+
+<div class="skillsection">
+
+ <h2>Skills</h2>
+ <p>TIE Pilot</p>
+ <div class="container">
+     <div class="skill c1">90%</div>
+ </div>
+
+ <p>Engineer</p>
+ <div class="container">
+     <div class="skill c2">70%</div>
+ </div>
+
+ <p>Sith Lord</p>
+ <div class="container">
+     <div class="skill c3">100%</div>
+ </div>
+
+ <p>Lightsaber dueling</p>
+ <div class="container">
+     <div class="skill c4">100%</div>
+ </div>
+
+ <p>Persuasion</p>
+ <div class="container">
+     <div class="skill c5">80%</div>
+ </div>
+
+ <p>Dark attitude</p>
+ <div class="container">
+     <div class="skill c6">100%</div>
+ </div>
+
+</div>
+
+
+
+</html>
