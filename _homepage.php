4d611440d62f0348312c0668d9133771b7a0e51c
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-03-17 15:50:26 +0100

    modification grid homepage + header navbar burger

--- _homepage.php
+++ _homepage.php
@@ -1,7 +1,15 @@
-<img src="assets/images/dv0.png" alt="Darth Vader" onmouseover= this.src="assets/images/dv1.png" onmouseout= this.src="assets/images/dv0.png" width="300" height="auto">
+<link rel="stylesheet" href="./assets/style.css">
 
-<h1>Darth Vader</h1>
+<section id=homepage>
 
-<p>
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod recusandae cupiditate vero quasi hic, maxime pariatur itaque corporis voluptatem laborum officiis voluptates, beatae architecto distinctio unde tempora atque consequuntur quos.
-</p>
+    
+
+    <h1>Darth Vader</h1>
+
+    <img src="assets/images/dv0.png" alt="Darth Vader" onmouseover=this.src="assets/images/dv1.png" onmouseout=this.src="assets/images/dv0.png" width="300" height="auto">
+
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.mpora atque consequuntur quos.
+    </p>
+
+</section>
