4f164e8956c566038de0e722a7bb853e76e7bbd3
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-03-18 10:38:43 +0100

    update contact

--- _contact.php
+++ _contact.php
@@ -1,1 +1,44 @@
-"hello contact"
+<link rel="stylesheet" href="./assets/style.css">
+
+<section id="contact">
+    <h2>Get in touch</h2>
+        <form>
+            <div id="getintouch"> 
+                <div class="form">
+                    <input type="text" placeholder="Name">  
+                    <input type="text" placeholder="Surname">   
+                    <input type="email" placeholder="@">
+                    <textarea placeholder="Your message"></textarea>
+                </div>
+                <div class="contact">
+                    <div class="callme">
+                        <div class="image">
+                            <div><img src="assets/images/phone.png" alt="Call me"></div>
+                        </div>
+                        <div class="text">
+                            <div><h3>Call me</h3></div>
+                            <div><p>800-5-5-21-1980</p></div>
+                        </div>
+                    </div>
+                    <div class="contactme">
+                        <div class="image">
+                            <div><img src="assets/images/email.png" alt="Contact me"></div>
+                        </div>
+                        <div class="text">
+                            <div><h3>Contact me</h3></div>
+                            <div><p>darth.vader@gmail.com</p></div>
+                        </div>
+                    </div>
+                    <div class="findme">
+                        <div class="image">
+                            <div><img src="assets/images/map.png" alt="Find me"></div>
+                        </div>
+                        <div class="text">
+                            <div><h3>Location</h3></div>
+                            <div><p>Death star - Level1</p></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+</section>
