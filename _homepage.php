e9bbe67d30a527277c8c4892faac852cc0c6ea90
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-03-18 09:13:18 +0100

    modification header

--- _homepage.php
+++ _homepage.php
@@ -2,14 +2,10 @@
 
 <section id=homepage>
 
-    
-
     <h1>Darth Vader</h1>
 
-    <img src="assets/images/dv0.png" alt="Darth Vader" onmouseover=this.src="assets/images/dv1.png" onmouseout=this.src="assets/images/dv0.png" width="300" height="auto">
+    <img src="assets/images/dv0.png" alt="Darth Vader" onmouseover=this.src="assets/images/dv1.png" onmouseout=this.src="assets/images/dv0.png" width="80%" height="auto">
 
-    <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.mpora atque consequuntur quos.
-    </p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.mpora atque consequuntur quos.</p>
 
 </section>
