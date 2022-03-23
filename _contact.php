3eec34542f0ea4e7d4f368042e4f979df6d968c4
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-03-23 16:36:00 +0100

    add alert + modif noms de fichiers

--- _contact.php
+++ _contact.php
@@ -1,20 +1,28 @@
-<link rel="stylesheet" href="./assets/style.css">
 
+<?php
+if (isset($_POST['id'])) echo $_POST['id'];
+?>
 <section id="contact">
     <h2>Get in touch</h2>
+    <div id="alert">
+        <a href="#">fermer</a>
+        <h1>alert</h1>
+        Hello <?php echo $_POST['fname'] ?> May the force be with you
+    </div>
     <div id="getintouch">
-        <form action="" method="post"> 
-            <div class="form">
-                <label for="fname">First name</label>
-                <input type="text" id="fname" name="fname" placeholder="Name">
-                <label for="lname">Last name</label>  
-                <input type="text" id="lname" name="lname" placeholder="Surname">  
-                <label for="email">E-mail</label> 
-                <input type="email" id="email" name="email" placeholder="@">
-                <label for="message">Message</label>
-                <textarea id="message" name="message" placeholder="Your message"></textarea>
-                <input class="bouton" type="submit" value="Submit">
-            </div>
+
+        <form action="#alert" method="post"></form>
+        <div class="form">
+            <label for="fname">First name</label>
+            <input type="text" id="fname" name="fname" placeholder="Name">
+            <label for="lname">Last name</label>
+            <input type="text" id="lname" name="lname" placeholder="Surname">
+            <label for="email">E-mail</label>
+            <input type="email" id="email" name="email" placeholder="@">
+            <label for="message">Message</label>
+            <textarea id="message" name="message" placeholder="Your message"></textarea>
+            <input class="bouton" type="submit" value="Submit">
+        </div>
         </form>
         <div class="contact">
             <div class="callme">
@@ -22,8 +30,12 @@
                     <div><img src="assets/images/phone.png" alt="Call me"></div>
                 </div>
                 <div class="text">
-                    <div><h3>Call me</h3></div>
-                    <div><p>800-5-5-21-1980</p></div>
+                    <div>
+                        <h3>Call me</h3>
+                    </div>
+                    <div>
+                        <p>800-5-5-21-1980</p>
+                    </div>
                 </div>
             </div>
             <div class="contactme">
@@ -31,8 +43,12 @@
                     <div><img src="assets/images/email.png" alt="Contact me"></div>
                 </div>
                 <div class="text">
-                    <div><h3>Contact me</h3></div>
-                    <div><p>darth.vader@gmail.com</p></div>
+                    <div>
+                        <h3>Contact me</h3>
+                    </div>
+                    <div>
+                        <p>darth.vader@gmail.com</p>
+                    </div>
                 </div>
             </div>
             <div class="findme">
@@ -40,8 +56,12 @@
                     <div><img src="assets/images/map.png" alt="Find me"></div>
                 </div>
                 <div class="text">
-                    <div><h3>Location</h3></div>
-                    <div><p>Death star - Level1</p></div>
+                    <div>
+                        <h3>Location</h3>
+                    </div>
+                    <div>
+                        <p>Death star - Level1</p>
+                    </div>
                 </div>
             </div>
         </div>
