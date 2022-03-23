0906006aa3ab1afcb09c70536037a4f651e8b873
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-03-23 12:24:18 +0100

    contact and portfolio update

--- _contact.php
+++ _contact.php
@@ -2,44 +2,48 @@
 
 <section id="contact">
     <h2>Get in touch</h2>
-        <form>
-            <div id="getintouch"> 
-                <div class="form">
-                    <input type="text" name="name" aria-label="name" placeholder="Name">  
-                    <input type="text" placeholder="Surname">   
-                    <input type="email" placeholder="@">
-                    <textarea placeholder="Your message"></textarea>
-                    <input class="bouton" type="submit" value="Submit">
-                </div>
-                <div class="contact">
-                    <div class="callme">
-                        <div class="image">
-                            <div><img src="assets/images/phone.png" alt="Call me"></div>
-                        </div>
-                        <div class="text">
-                            <div><h3>Call me</h3></div>
-                            <div><p>800-5-5-21-1980</p></div>
-                        </div>
-                    </div>
-                    <div class="contactme">
-                        <div class="image">
-                            <div><img src="assets/images/email.png" alt="Contact me"></div>
-                        </div>
-                        <div class="text">
-                            <div><h3>Contact me</h3></div>
-                            <div><p>darth.vader@gmail.com</p></div>
-                        </div>
-                    </div>
-                    <div class="findme">
-                        <div class="image">
-                            <div><img src="assets/images/map.png" alt="Find me"></div>
-                        </div>
-                        <div class="text">
-                            <div><h3>Location</h3></div>
-                            <div><p>Death star - Level1</p></div>
-                        </div>
-                    </div>
-                </div>
+    <div id="getintouch">
+        <form action="" method="post"> 
+            <div class="form">
+                <label for="fname">First name</label>
+                <input type="text" id="fname" name="fname" placeholder="Name">
+                <label for="lname">Last name</label>  
+                <input type="text" id="lname" name="lname" placeholder="Surname">  
+                <label for="email">E-mail</label> 
+                <input type="email" id="email" name="email" placeholder="@">
+                <label for="message">Message</label>
+                <textarea id="message" name="message" placeholder="Your message"></textarea>
+                <input class="bouton" type="submit" value="Submit">
             </div>
         </form>
+        <div class="contact">
+            <div class="callme">
+                <div class="image">
+                    <div><img src="assets/images/phone.png" alt="Call me"></div>
+                </div>
+                <div class="text">
+                    <div><h3>Call me</h3></div>
+                    <div><p>800-5-5-21-1980</p></div>
+                </div>
+            </div>
+            <div class="contactme">
+                <div class="image">
+                    <div><img src="assets/images/email.png" alt="Contact me"></div>
+                </div>
+                <div class="text">
+                    <div><h3>Contact me</h3></div>
+                    <div><p>darth.vader@gmail.com</p></div>
+                </div>
+            </div>
+            <div class="findme">
+                <div class="image">
+                    <div><img src="assets/images/map.png" alt="Find me"></div>
+                </div>
+                <div class="text">
+                    <div><h3>Location</h3></div>
+                    <div><p>Death star - Level1</p></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
