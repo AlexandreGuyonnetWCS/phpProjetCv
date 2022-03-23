2c6e68c6c4058d7423aae48b78f574f81215288c
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-03-23 11:30:05 +0100

    add flex in background

--- _background.php
+++ _background.php
@@ -3,82 +3,46 @@
 <section id="background">
 
     <h2>Background</h2>
-
-    <img class="img-sabre" src="assets/images/sabrerouge.png" alt="Sabre rouge">
+<!-- 
+    <img class="img-sabre" src="assets/images/sabrerouge.png" alt="Sabre rouge"> -->
 
     <h3>Work history</h3>
-     <div class="job"> 
-        <div class="about">
-            <h4>Galactic Empire</h4>
-        </div>
-        <div class="task">
-            <ul>
-                <li>Captured Princess Leia</li>
-                <li>Oversaw construction of the Death Star</li>
-                <li>Reduced operating cost by 36% through mass executions</li>
-            </ul>
-            
-            </div>
+    <div class="job">
+        <h4>Galactic Empire</h4>
+        <ul>
+            <li>Captured Princess Leia</li>
+            <li>Oversaw construction of the Death Star</li>
+            <li>Reduced operating cost by 36% through mass executions</li>
+        </ul></br>
+
+        <h4>Jedi Council</h4>
+        <ul>
+            <li>Made coffe for Yoda</li>
+            <li>Trained jedi padawans</li>
+            <li>Maintened peace</li>
+        </ul></br>
     </div>
 
-    <div class="job">
-        <div class="about">
-            <h4>Jedi Council</h4>
-        </div>
-        <div class="task">
-            <ul>
-                <li>Made coffe for Yoda</li>
-                <li>Trained jedi padawans</li>
-                <li>Maintened peace</li>
-            </ul>       
-        </div>
-    </div>
-    
-    <img class="img-sabre" src="assets/images/sabrerouge.png" alt="Sabre rouge">
+    <!-- <img class="img-sabre" src="assets/images/sabrerouge.png" alt="Sabre rouge"> -->
 
     <h3>Education</h3>
 
     <div class="job">
-        <div class="about">
-            <h4>Institute of Dark Side Studies</h4>
-        </div>
-        <div class="task">
-            <ul>
-                <li>Strategic Galaxy Domination</li>
-                <li>Dark Side Force</li>
-                <li>Handsfree Strangulation Techniques</li>
-                <li>Cyborg Lifestyle</li>
-            </ul>       
+        <h4>Institute of Dark Side Studies</h4>
+        <ul>
+            <li>Strategic Galaxy Domination</li>
+            <li>Dark Side Force</li>
+            <li>Handsfree Strangulation Techniques</li>
+            <li>Cyborg Lifestyle</li>
+        </ul></br>
+        <h4>Obi-Wan Kenobi Jedi training</h4>
+        <ul>
+            <li>Ethcs of force use</li>
+            <li>Lightsaber wielding and dueling</li>
+            <li>Jedi history</li>
+        </ul>
     </div>
-        
-    <div class="job">
-        <div class="about">
-            <h4>Obi-Wan Kenobi Jedi training</h4>
-        </div>
-        <div class="task">
-            <ul>
-                <li>Ethcs of force use</li>
-                <li>Lightsaber wielding and dueling</li>
-                <li>Jedi history</li>
-            </ul>       
-    </div>
-
-    <img class="img-sabre" src="assets/images/sabrerouge.png" alt="Sabre rouge">
+<!-- 
+    <img class="img-sabre" src="assets/images/sabrerouge.png" alt="Sabre rouge"> -->
 
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
