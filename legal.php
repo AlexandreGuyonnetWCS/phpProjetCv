3aa4d12038ae2cd9a5247cf6dec2092d811bb65f
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-03-31 15:08:21 +0200

    fix legal notice for github pages

---
+++ legal.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+<section id="legal">
+  <div id="body">
+    <div class="wrapper">
+      <div class="scroll-text">
+        <h1>Legal Notice</h1>
+        <h2>DéPhant</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+        <a href="/index.php"> Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me, Click Me,  </a>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
